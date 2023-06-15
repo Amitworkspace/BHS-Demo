caef7a81570f8b6c5c331fc07bdf8f647035dd03
Amitworkspace <65153068+Amitworkspace@users.noreply.github.com>
2023-06-15 20:25:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     source_image_reference {
         publisher = "Canonical"
         offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
+        sku       = "22.04-LTS"
         version   = "latest"
     }
 
