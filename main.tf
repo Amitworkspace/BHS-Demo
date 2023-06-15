577228e67c882e585deb243a28173ea3dfac72a1
Amitworkspace <65153068+Amitworkspace@users.noreply.github.com>
2023-06-15 20:32:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,11 +22,22 @@ terraform {
   }
 }
 
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "BHSDemo"
+    location = "us-east"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create virtual network
 resource "azurerm_virtual_network" "myterraformnetwork" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "us-east"
-    resource_group_name = "BHSDemo"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
 
     tags = {
         environment = "Terraform Demo"
@@ -36,7 +47,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
 # Create subnet
 resource "azurerm_subnet" "myterraformsubnet" {
     name                 = "mySubnet"
-    resource_group_name  = "BHSDemo"
+    resource_group_name  = azurerm_resource_group.myterraformgroup.name
     virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
     address_prefixes       = ["10.0.1.0/24"]
 }
@@ -45,7 +56,7 @@ resource "azurerm_subnet" "myterraformsubnet" {
 resource "azurerm_public_ip" "myterraformpublicip" {
     name                         = "myPublicIP"
     location                     = "us-east"
-    resource_group_name          = "BHSDemo"
+    resource_group_name          = azurerm_resource_group.myterraformgroup.name
     allocation_method            = "Dynamic"
 
     tags = {
@@ -57,7 +68,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
 resource "azurerm_network_security_group" "myterraformnsg" {
     name                = "myNetworkSecurityGroup"
     location            = "us-east"
-    resource_group_name = "BHSDemo"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
 
     security_rule {
         name                       = "SSH"
@@ -80,7 +91,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
 resource "azurerm_network_interface" "myterraformnic" {
     name                      = "myNIC"
     location                  = "us-east"
-    resource_group_name       = "BHSDemo"
+    resource_group_name       = azurerm_resource_group.myterraformgroup.name
 
     ip_configuration {
         name                          = "myNicConfiguration"
@@ -100,6 +111,29 @@ resource "azurerm_network_interface_security_group_association" "example" {
     network_security_group_id = azurerm_network_security_group.myterraformnsg.id
 }
 
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = azurerm_resource_group.myterraformgroup.name
+    }
+
+    byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+    location                    = "us-east"
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
 # Create (and display) an SSH key
 resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
@@ -114,10 +148,10 @@ output "tls_private_key" {
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
     name                  = "myVM"
     location              = "us-east"
-    resource_group_name   = "BHSDemo"
+    resource_group_name   = azurerm_resource_group.myterraformgroup.name
     network_interface_ids = [azurerm_network_interface.myterraformnic.id]
     size                  = "Standard_DS1_v2"
-}
+
     os_disk {
         name              = "myOsDisk"
         caching           = "ReadWrite"
@@ -140,6 +174,13 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
         public_key     = file("~/.ssh/id_rsa.pub")
     }
 
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
 	    
 	connection {
         host = self.public_ip_address
@@ -149,12 +190,16 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
         timeout = "4m"
         agent = false
     }
+	
+	provisioner "file" {
+        source = "example_file.txt"
+        destination = "/tmp/example_file.txt"
+    }
+
     provisioner "remote-exec" {
         inline = [
           "sudo apt-get update",
-          "sudo apt-get install docker.io -y",
-          "sudo docker run -d -p 80:80 -name=nginx nginx",
-          "sudo docker exec -it nginx /bin/bash/",
-          "echo 'Hello BHS'> /var/www/html/index.html"
+          "sudo apt-get install docker.io -y"
+          "sudo docker run -d -p 80:80 httpd"
         ]
     }
