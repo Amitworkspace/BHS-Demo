2a9de4b9fc9dd386468c5a8d37db0b4d7795374d
Amitworkspace <65153068+Amitworkspace@users.noreply.github.com>
2023-06-16 15:10:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,69 @@
-provider "azurerm" { 
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
   features {}
 }
 
-// resource "azurerm_resource_group" "example" {
-//   name     = "example-resource-group"
-//   location = "West Europe"
-// }
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "BHSDemo"
+#     storage_account_name = "bhsdemostorage"
+#     container_name       = "tfstatefile"
+#     key                  = "prod.terraform.tfstate"
+#   }
+# }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "east us"
-  resource_group_name = "BHSDemo"
+# # Create a resource group if it doesn't exist
+# resource "azurerm_resource_group" "myterraformgroup" {
+#     name     = "BHSDemo"
+#     location = "east us"
+
+#     tags = {
+#         environment = "Terraform Demo"
+#     }
+# }
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myvnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "east us"
+    resource_group_name = "BHSDemo"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = "BHSDemo"
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = "BHSDemo"
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes       = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-publicip"
-  location            = "east us"
-  resource_group_name = "BHSDemo"
-  allocation_method   = "Dynamic"
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "mypublicIP"
+    location                     = "east us"
+    resource_group_name          = "BHSDemo"
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = "east us"
-  resource_group_name = "BHSDemo"
-
-  ip_configuration {
-    name                          = "example-ipc"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "myterraformnsg" {
     name                = "myNetworkSecurityGroup"
@@ -73,68 +97,6 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     }
 }
 
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"
-  location              = "east us"
-  resource_group_name   = "BHSDemo"
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "example-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "examplevm"
-    admin_username = "adminuser"
-    // admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-  }
-
-   admin_ssh_key {
-        username       = "admineuser"
-        public_key     = tls_private_key.example_ssh.public_key_openssh
-    }
-
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-	    
-	connection {
-        host = self.public_ip_address
-        user = "azureuser"
-        type = "ssh"
-        private_key = tls_private_key.example_ssh.public_key_openssh
-        timeout = "4m"
-        agent = false
-    }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello, World!'"
-    ]
-  }
-}
-
-
 # Create network interface
 resource "azurerm_network_interface" "myterraformnic" {
     name                      = "myNIC"
@@ -159,6 +121,29 @@ resource "azurerm_network_interface_security_group_association" "example" {
     network_security_group_id = azurerm_network_security_group.myterraformnsg.id
 }
 
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = "BHSDemo"
+    }
+
+    byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = "BHSDemo"
+    location                    = "east us"
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
 # Create (and display) an SSH key
 resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
@@ -167,4 +152,63 @@ resource "tls_private_key" "example_ssh" {
 output "tls_private_key" { 
     value = tls_private_key.example_ssh.private_key_pem 
     sensitive = true
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "east us"
+    resource_group_name   = "BHSDemo"
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    size                  = "Standard_DS1_v2"
+
+
+    os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "22.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "myvm"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username       = "azureuser"
+        public_key     = tls_private_key.example_ssh.public_key_openssh
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+	    
+	connection {
+        host = self.public_ip_address
+        user = "azureuser"
+        type = "ssh"
+        private_key = tls_private_key.example_ssh.public_key_openssh
+        timeout = "4m"
+        agent = false
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "sudo apt-get update",
+          "sudo apt-get install docker.io -y",
+          "sudo docker run -d -p 80:80 -name=nginx nginx",
+          "sudo docker exec -it nginx /bin/bash/",
+          "echo 'Hello BHS'> /var/www/html/index.html"
+        ]
+    }
 }
